8c39f97bf4bc63641961d4092be2dc2d7f580440
Dan Esparza <danesparza@cagedtornado.com>
2024-01-27 09:19:52 -0500

    Update prereq.sh

--- prereq.sh
+++ prereq.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -s --compressed "https://danesparza.github.io/package-repo/KEY.gpg" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/package-repo.gpg >/dev/null
-sudo curl -s --compressed -o /etc/apt/sources.list.d/package-repo.list "https://danesparza.github.io/package-repo/package-repo.list"
+curl -s --compressed "https://packages.cagedtornado.com/KEY.gpg" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/package-repo.gpg >/dev/null
+sudo curl -s --compressed -o /etc/apt/sources.list.d/package-repo.list "https://packages.cagedtornado.com/package-repo.list"
 sudo apt update
